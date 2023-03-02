fe213a772d18557604afc3b86dc0c9bee81f2248
Ayush <ayushpersonal2310@gmail.com>
2023-03-02 11:23:48 +0530

    Added some routes

--- server/db.js
+++ server/db.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
 
 const Connection =  (username, password) => {
-    mongoose.connect(`mongodb+srv://${username}:${password}@ecommerce-web.urgvvgi.mongodb.net/?retryWrites=true&w=majority`).then(()=>{
+    const URL = `mongodb+srv://${username}:${password}@ecommerce-web.urgvvgi.mongodb.net/test`
+    mongoose.connect(URL).then(()=>{
         console.log("Sucessfully Connected with db")
     }).catch((err)=>{
         console.log("Error occured")
         console.log(err)
     })
 }
+
 
 
 module.exports = Connection
---
+++ server/userSchema.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema({
+    firstName : {
+        type : String,
+        required : true,
+        trim : true,
+        min  :5 ,
+        max : 20
+    },
+    lastName : {
+        type : String,
+        required : true,
+        trim : true,
+        min  :5 ,
+        max : 20
+    },
+    email : {
+        type : String,
+        required : true,
+        trim : true,
+        unique : true ,
+        lowercase : true
+    },
+    userName : {
+        type : String,
+        required : true,
+        trim : true,
+        unique: true,
+        index: true,
+        lowercase : true
+    },
+    password : {
+        type : String ,
+        required : true,
+    },
+    phone : {
+        type : String ,
+        required : true
+    }
+})
+const User = mongoose.model('user', userSchema)
+module.exports = User
