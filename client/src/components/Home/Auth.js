f00c083e164c372bcd7dc7d1675684f306783c08
Ayush <ayushpersonal2310@gmail.com>
2023-03-12 23:41:11 +0530

    Home Layout

--- client/src/components/Home/Auth.js
+++ client/src/components/Home/Auth.js
@@ -44,98 +44,97 @@ const Auth = () => {
     }
 
     return (
-        <section className="vh-100" style={{ backgroundColor: "#eee" }}>
+        <section className="vh-100" style={{ backgroundColor: "black" }}>
             <div className='container h-100'>
-                <div className='row d-flex justify-content-center align-items-center h-100'>
-                    <div className='col-lg-12 col-xl-11'>
-                    <div className="card text-black" style={{borderRadius: "25px"}}>
-                            <div className="card-body p-md-5">
+                <div className='row d-flex justify-content-center align-items-center h-100' >
+                    <div className='col-lg-12 col-xl-11' >
+                        <div className="card text-black" style={{ borderRadius: "25px" }}>
+                            <div className="card-body p-md-5" >
                                 <div className="row justify-content-center">
-                                        <p className='text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4'>{isSignUp ? 'Sign Up' : 'Sign In'}</p>
-                                        <form className="mx-1 mx-md-4" onSubmit={handleSubmit}>
-                                            {
-                                                isSignUp && (
-                                                    <div className="d-flex flex-row align-items-center mb-4">
-                                                        <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                                                        <div className="form-outline flex-fill mb-0">
-                                                            <input
-                                                                className='form-control'
-                                                                name='name'
-                                                                type='text'
-                                                                placeholder='Name'
-                                                                onChange={handleChange}
-                                                                required
-                                                            />
-                                                            {/* <label htmlFor='name'>Name</label> */}
-                                                        </div>
+                                    <p className='text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4'>{isSignUp ? 'Sign Up' : 'Sign In'}</p>
+                                    <form className="mx-1 mx-md-4"  onSubmit={handleSubmit}>
+                                        {
+                                            isSignUp && (
+                                                <div className="d-flex flex-row align-items-center mb-4">
+                                                    <i class="bi bi-person-circle mr-2"></i><div className="form-outline flex-fill mb-0">
+                                                        <input
+                                                            className='form-control'
+                                                            name='name'
+                                                            type='text'
+                                                            placeholder='Name'
+                                                            onChange={handleChange}
+                                                            required
+                                                        />
+                                                        {/* <label htmlFor='name'>Name</label> */}
                                                     </div>
-
-                                                )
-                                            }
-                                            <div className="d-flex flex-row align-items-center mb-4">
-                                                <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                                                <div className="form-outline flex-fill mb-0">
-                                                    <input
-                                                        className='form-control'
-                                                        name='email'
-                                                        type='text'
-                                                        placeholder='Email'
-                                                        onChange={handleChange}
-                                                        required
-                                                    />
-                                                    {/* <label htmlFor='email'>Email</label> */}
                                                 </div>
-                                            </div>
 
-                                            {
-                                                isSignUp && (
-                                                    <div className="d-flex flex-row align-items-center mb-4">
-                                                        <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                                                        <div className="form-outline flex-fill mb-0">
-                                                            <input
-                                                                className='form-control'
-                                                                name='phone'
-                                                                type='text'
-                                                                placeholder='Phone'
-                                                                onChange={handleChange}
-                                                                required
-                                                            />
-                                                            {/* <label htmlFor='phone'>Phone</label> */}
-                                                        </div>
+                                            )
+                                        }
+                                        <div className="d-flex flex-row align-items-center mb-4">
+                                            <i class="bi bi-envelope mr-2"></i>
+                                            <div className="form-outline flex-fill mb-0">
+                                                <input
+                                                    className='form-control'
+                                                    name='email'
+                                                    type='text'
+                                                    placeholder='Email'
+                                                    onChange={handleChange}
+                                                    required
+                                                />
+                                                {/* <label htmlFor='email'>Email</label> */}
+                                            </div>
+                                        </div>
+
+                                        {
+                                            isSignUp && (
+                                                <div className="d-flex flex-row align-items-center mb-4">
+                                                    <i class="bi bi-phone mr-2"></i>
+                                                    <div className="form-outline flex-fill mb-0">
+                                                        <input
+                                                            className='form-control'
+                                                            name='phone'
+                                                            type='text'
+                                                            placeholder='Phone'
+                                                            onChange={handleChange}
+                                                            required
+                                                        />
+                                                        {/* <label htmlFor='phone'>Phone</label> */}
                                                     </div>
-
-                                                )
-                                            }
-
-                                            <div className="d-flex flex-row align-items-center mb-4">
-                                                <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                                                <div className="form-outline flex-fill mb-0">
-                                                    <input
-                                                        className='form-control'
-                                                        name='password'
-                                                        type='password'
-                                                        placeholder='Password'
-                                                        onChange={handleChange}
-                                                        required
-                                                    />
-                                                    {/* <label htmlFor='password'>Password</label> */}
                                                 </div>
+
+                                            )
+                                        }
+
+                                        <div className="d-flex flex-row align-items-center mb-4">
+                                        <i class="bi bi-key-fill mr-2"></i>
+                                            <div className="form-outline flex-fill mb-0">
+                                                <input
+                                                    className='form-control'
+                                                    name='password'
+                                                    type='password'
+                                                    placeholder='Password'
+                                                    onChange={handleChange}
+                                                    required
+                                                />
+                                                {/* <label htmlFor='password'>Password</label> */}
                                             </div>
-                                            <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                                <button type="submit" className="btn btn-primary btn-lg">
-                                                    {
-                                                        isSignUp
-                                                            ? 'Sign up'
-                                                            : 'Sign In'
-                                                    }</button>
-                                            </div>
-                                        </form>
-                                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                                        </div>
+                                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                            <button type="submit" className="btn btn-primary btn-lg">
+                                                {
+                                                    isSignUp
+                                                        ? 'Sign up'
+                                                        : 'Sign In'
+                                                }</button>
+                                        </div>
+                                    </form>
+                                    {/* <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
 
                                         <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
                                             className="img-fluid" alt="Sample image" />
 
-                                    </div>
+                                    </div> */}
 
                                 </div>
                             </div>
@@ -157,7 +156,7 @@ const Auth = () => {
                                     }
                                 </span>
                             </p>
-                        </div>    
+                        </div>
                     </div>
                 </div>
             </div>
--- client/src/components/Home/Header.js
+++ client/src/components/Home/Header.js
@@ -1,4 +1,4 @@
-import {Box, styled} from "@mui/material"
+import {Box, styled, List, ListItem} from "@mui/material"
 const Main = styled(Box)({
     height : "100vh",
     backgroundColor : "black",
@@ -6,20 +6,24 @@ const Main = styled(Box)({
     fontWeight : "400",
 })
 
+
+
 const Header = () => {
     return (
-        <Main className=" col-lg-4">
-            <ul>
-                <li>
-                    Twitter_logo
-                </li>
-                <li>
-                    Tweet_logo
-                </li>
-                <li>
-                    Setting
-                </li>
-            </ul>
+        <Main className=" col-lg-3">
+            <List className="d-flex flex-column justify-content-center align-items-center" style={{ listStyle : "none" }}>
+                <ListItem className="ml-5 mb-3">
+                    <i class="bi bi-twitter"></i>
+                </ListItem>
+                <ListItem className="ml-5 mb-3">
+                    <i class="bi bi-hash"></i>
+                    Explore
+                </ListItem>
+                <ListItem className="ml-5 mb-3">
+                    <i class="bi bi-gear"></i>
+                    Settings
+                </ListItem>
+            </List>
         </Main>
         
     )
