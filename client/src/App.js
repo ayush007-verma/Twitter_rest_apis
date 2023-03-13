73298c50328430697c115663193388ed6bc29045
Anubhav530 <anubhav3516.be20@chitkara.edu.in>
2023-03-13 20:09:20 +0530

    conflict resolve

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,36 @@
-import './App.css';
-import Auth from './components/Auth';
-import Cookies from 'universal-cookie';
-import PasswordReset from './components/PasswordReset';
+// import './App.css';
+// import Auth from './components/Auth';
+// import Cookies from 'universal-cookie';
+// import PasswordReset from './components/PasswordReset';
 
-const cookies = new Cookies();
+// const cookies = new Cookies();
 
-const authToken = cookies.get("authToken");
+// const authToken = cookies.get("authToken");
 
 
-const App = () => {
+// const App = () => {
   
-  if (!authToken) {
-    return <Auth />
-  }
+//   if (!authToken) {
+//     return <Auth />
+//   }
 
+//   return (
+//     <div className="App">
+//       <PasswordReset />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import './App.css';
+
+import Home from "./components/Home/Home"
+
+function App() {
   return (
     <div className="App">
-      <PasswordReset />
+      <Home/> 
     </div>
   );
 }
