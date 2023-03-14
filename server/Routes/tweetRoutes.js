e24a658bad04d9bf8649e53552a403a17742f359
Ayush <ayushpersonal2310@gmail.com>
2023-03-14 12:18:13 +0530

    Tweet Apis CRUD

--- server/Routes/tweetRoutes.js
+++ server/Routes/tweetRoutes.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { verifyUser } = require('../verifyUser')
-const { createTweet, deleteTweet, likeOrDislike } = require('../controller/tweet')
+const { createTweet, deleteTweet, likeOrDislike, getTweet, getTweetById } = require('../controller/tweet')
+
+// GET ALL TWEETS
+router.get('/getTweets', getTweet)
+
+// GET ALL TWEETS BY USER ID
+router.get('/getTweets/:id', getTweetById)
 
 // CREATE TWEET ROUTE
 router.post('/', verifyUser, createTweet)
